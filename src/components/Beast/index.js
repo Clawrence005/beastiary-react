cad2897e4ac603a54308f05c952efc7a1eb26fe1
colin <clawrence005@yahoo.com>
2020-01-27 01:08:53 -0600

    beginning style

--- src/components/Beast/index.js
+++ src/components/Beast/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import "./style.css"
 function Beast(props) {
   return (
     <div className="beast-div">
       <h2>{props.name}</h2>
 
       <img
+        className="beast-image"
         key={props.id}
         src={props.image}
         alt={"image-" + props.name}
--- src/containers/BeastGrid/index.js
+++ src/containers/BeastGrid/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import "./styles.css";
+import "./style.css";
 
 function BeastGrid(props) {
   return <div className="beast-grid">
