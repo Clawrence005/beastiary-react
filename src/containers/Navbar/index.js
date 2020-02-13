56fcb96e775a1d70d2e58c70f376e58cce65d65e
colin <clawrence005@yahoo.com>
2020-02-13 09:51:00 -0600

    adding styling variables

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 // import NavBurger from '../../components/NavComponents/NavBurger';
 // import NavName from '../../components/NavComponents/NavName';
 
