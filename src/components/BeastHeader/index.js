9d0bad8920902e86422cabef08c59e48d454c6f4
colin <clawrence005@yahoo.com>
2020-02-13 10:03:28 -0600

    setting default padding and margin to 0

--- src/components/BeastHeader/index.js
+++ src/components/BeastHeader/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './style.css';
 
 function BeastHeader(props) {
-  return <h2>{props.name}</h2>
+  return <h2 className="beast-header">{props.name}</h2>
 }
 
 export default BeastHeader;
